1b6959aed520b3a93935286c0732f6079cc7b625
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-11-07 19:35:10 +0100

    add inline test

--- Labs/B - Language Features/Interfaces/define.test.ts
+++ Labs/B - Language Features/Interfaces/define.test.ts
@@ -4,7 +4,6 @@ describe('interface#definition', () => {
     it('define', () => {
         // TODO : define the interface Vehicule (Note : Vehicule should not be empty)
 
-
         class Car implements Vehicule {
             name:string;
             speed:number;
@@ -12,12 +11,23 @@ describe('interface#definition', () => {
                 this.name=name;
             }
         }
-
         
         var lbl=new Car();
         lbl.name='mylabel';
         chai.expect(lbl).to.be.an('object');
         chai.expect(lbl.name).to.be.equal('mylabel');
         chai.expect(lbl.select()).to.be.true;;
+    })
+    it('inline', () => {
+        var car :{ 
+            name:string,
+            speed:number
+        }
+
+        // TODO : instanciate a new car
+
+        chai.expect(car).to.be.an('object');
+        chai.expect(car.name).to.be.equal('Kit');
+        chai.expect(car.speed).to.be.equal(75);
     })
 })
