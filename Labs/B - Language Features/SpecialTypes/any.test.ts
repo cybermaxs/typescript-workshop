b70108e4e46f0c60b4d71707c72a71a802c1f8d5
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-11-15 22:45:11 +0100

    fix any

--- Labs/B - Language Features/SpecialTypes/any.test.ts
+++ Labs/B - Language Features/SpecialTypes/any.test.ts
@@ -13,7 +13,7 @@ describe('any', ()=> {
         var a_actual = MyToString([1,2,3]);
         chai.expect(a_actual).to.be.equal('1,2,3');
 
-        var n_actual = MyToString('123');
-        chai.expect(n_actual).to.be.equal('123');
+        var s_actual = MyToString('123');
+        chai.expect(s_actual).to.be.equal('123');
     })
 })
