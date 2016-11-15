0ccbbe5530e56cda8b8b680ceec45877b86acbdc
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-11-15 22:44:53 +0100

    any answer

--- Labs/B - Language Features/SpecialTypes/any.test.ts
+++ Labs/B - Language Features/SpecialTypes/any.test.ts
@@ -4,7 +4,7 @@ describe('any', ()=> {
     it('any', ()=> {
         // TODO : fix the test by using the any type annotation
         function MyToString(obj:any) {
-            return obj.ToString();
+            return obj.toString();
         }
 
         var n_actual = MyToString(123);
