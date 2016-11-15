a3dd5d05abf64f9aa9750625d99165ce10c3f3bf
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-11-15 22:52:36 +0100

    fix vars

--- Labs/B - Language Features/Destructuring/destructuring.array.ts
+++ Labs/B - Language Features/Destructuring/destructuring.array.ts
@@ -4,22 +4,22 @@ describe('Array destructuring', () => {
     it('simple', () => {
         let input = [1, 2];
         // TODO : assign input[0] to x and input[1] to by using a single ligne of code
-        /* ...FILL_ME_IN... */
-        chai.expect(x).to.be.equal(20)
-        chai.expect(y).to.be.equal(10)
+        var [x,y]=input;
+        chai.expect(x).to.be.equal(1)
+        chai.expect(y).to.be.equal(2)
     })
     it('swap variables', () => {
         var x = 10
         var y = 20; 
         // TODO : swap variables without a third variable and by using a single ligne of code
-        /* ...FILL_ME_IN... */
+        var [x,y]=[y,x];
         chai.expect(x).to.be.equal(20)
         chai.expect(y).to.be.equal(10)
     })
     it('rest parameters', () => {
         var inputs = [10,20,30,40];
         // TODO : assign two first values to x/y and the others parameters to an array called 'remainging' with a single ligne of code
-        /* ...FILL_ME_IN... */
+        var [x,y,...remaining]=inputs;
         chai.expect(x).to.be.equal(10)
         chai.expect(y).to.be.equal(20)
         chai.expect(remaining).to.be.deep.equal([30,40]);
@@ -27,7 +27,7 @@ describe('Array destructuring', () => {
     it('ignore parameters', () => {
         var inputs = [10,20,30,40];
         // TODO : assign first value to x, skip the second item and the others parameters to an array called 'remainging' with a single ligne of code
-        /* ...FILL_ME_IN... */
+        var [x,,...remaining]=inputs;
         chai.expect(x).to.be.equal(10)
         chai.expect(remaining).to.be.deep.equal([30,40]);
     })
