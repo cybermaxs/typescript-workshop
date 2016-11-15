7114f47d062858c59b2fe96c8a52ddb59d202d58
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-11-15 22:18:53 +0100

    Basic + Special Answers

--- Labs/B - Language Features/BasicTypes/number.test.ts
+++ Labs/B - Language Features/BasicTypes/number.test.ts
@@ -3,14 +3,14 @@ import * as chai from 'chai'
 describe('number', ()=> {
     it('assignToLet', ()=> {
         // TODO : assign the correct value to this variable to fix this test
-        let thenumber = "12";
+        let thenumber = 42;
 
         chai.expect(thenumber).to.be.an('number');
         chai.expect(thenumber).to.be.equal(42);
     })
     it('assignToVar', ()=> {
         // TODO : review the compiler error, fix it and fix the test
-        var thenumber:number = "12";
+        var thenumber:number = 12;
 
         chai.expect(thenumber).to.be.an('number');
         chai.expect(thenumber).to.be.equal(12);
@@ -20,21 +20,21 @@ describe('number', ()=> {
         let opeB = 2;
 
         // TODO : assign the correct value to sum variable to fix this test
-        let sum = -1; 
+        let sum = opeA+opeB; 
 
         chai.expect(sum).to.be.an('number');
         chai.expect(sum).to.be.equal(3);
     })
     it('binary', ()=> {
         // TODO : assign the correct value (binary) to binary variable to fix this test
-        let binary:number = 0b000; 
+        let binary:number = 0b011; 
 
         chai.expect(binary).to.be.an('number');
         chai.expect(binary).to.be.equal(3);
     })
     it('hexa', ()=> {
         // TODO : assign the correct value (hexa) to variable to fix this test
-        let binary:number = 0xAAA; 
+        let binary:number = 0x011; 
 
         chai.expect(binary).to.be.an('number');
         chai.expect(binary).to.be.equal(17);
--- Labs/B - Language Features/BasicTypes/string.test.ts
+++ Labs/B - Language Features/BasicTypes/string.test.ts
@@ -3,13 +3,13 @@ import * as chai from 'chai'
 describe('Simple Strings', () => {
     it('defineUsingSimpleQuotes', () => {
         // TODO : define a string using simple quotes '
-        var s = 'FILL_Me_IN';
+        var s = 'I\'m a string';
         chai.expect(s).to.be.an('string');
         chai.expect(s).to.be.equal("I'm a string")
     })
     it('defineUsingDoubledQuotes', () => {
         // TODO : define a string using double quotes "
-        var s = "FILL_Me_IN";
+        var s = "I'm a string";
         chai.expect(s).to.be.an('string');
         chai.expect(s).to.be.equal("I'm a string")
     })
@@ -19,13 +19,13 @@ describe('Template Strings', () => {
     it('interpolated', () => {
         // TODO : create an interpolated string (note : do not use the + operator)
         var tag = "Carl";
-        var s = `Hello, my name is FILL_ME_IN.`;
+        var s = `Hello, my name is ${tag}.`;
         chai.expect(s).to.be.an('string');
         chai.expect(s).to.be.equal("Hello, my name is Carl.")
     })
     it('interpolatedExpression', () => {
         // TODO : define an expression in the interpolated string ( do not use 2)
-        var s = `1 and 1 make FILL_ME_IN`;
+        var s = `1 and 1 make ${1+1}`;
 
         chai.expect(s).to.be.an('string');
         chai.expect(s).to.be.equal("1 and 1 make 2")
@@ -34,7 +34,7 @@ describe('Template Strings', () => {
 describe('String Literal', () => {
     it('basic', () => {
         // TODO : define a string literal 'visibility' allowing only two states : 'Visible' and 'Hidden'
-
+        var visibility : 'Visible'|'Hidden';
         visibility = 'Visible';
         chai.expect(visibility).to.be.an('string');
         chai.expect(visibility).to.be.equal('Visible');
@@ -49,11 +49,11 @@ describe('String Literal', () => {
         // TODO : replace FILL_ME_IN by allowed string value to fix this test
         type visibility = 'Visible' | 'Hidden';
 
-        var vis: visibility = 'FILL_ME_IN';
+        var vis: visibility = 'Visible';
         chai.expect(vis).to.be.an('string');
         chai.expect(vis).to.be.equal('Visible');
 
-        var hid: visibility = 'FILL_ME_IN';
+        var hid: visibility = 'Hidden';
         chai.expect(hid).to.be.an('string');
         chai.expect(hid).to.be.equal('Hidden');
         // note : transpiled js is still valid, even if we have a ts warning
