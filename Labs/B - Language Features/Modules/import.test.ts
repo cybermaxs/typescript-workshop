89eca61e79304a2eca9ff0a86cb7af2b4d2554b3
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2016-11-16 08:19:09 +0100

    modules answers

--- Labs/B - Language Features/Modules/import.test.ts
+++ Labs/B - Language Features/Modules/import.test.ts
@@ -1,5 +1,6 @@
 import * as chai from 'chai'
-
+import {Calculator} from './Calculators/Calculator'
+import DefaultCalculator from './Calculators/AddCalculator'
 // IMPORTANT NOTE : tests should NOT be changed 
 // we're working with import directives ONLY, so nothing should be changed below this comment
 
@@ -8,8 +9,13 @@ describe('import', () => {
         // TODO : import the interface defined in calculator and implements your own calculator
         // Tip : import should be at he beginning of the file
         class UniverseCalculator implements Calculator {
+<<<<<<< 26f965dfde44432461eec8c5b773c651abdb4e87
             Calculate(a:number, b:number) {
               return FILL_ME_IN;
+=======
+          Calculate(a:number, b:number) {
+              return 42;
+>>>>>>> modules answers
           }
         }
         var mycalculator = new UniverseCalculator();
